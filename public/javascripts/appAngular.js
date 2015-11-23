b88d97ee32478eecb88f83bd31983036b2ffea42
TAmyG <tamy.vivas@gmail.com>
2015-11-22 19:20:03 -0600

    consumo de api rest e implementación en controlador y factory de angular

--- public/javascripts/appAngular.js
+++ public/javascripts/appAngular.js
@@ -14,39 +14,62 @@ angular.module('appTareas', ['ui.router'])
 
         $urlRouterProvider.otherwise('alta');
     })
-    .factory('comun', function() {
+
+    .factory('comun', function($http) {
         var comun = {}
 
-        comun.tareas = [{
-            nombre: 'Comprar comida',
-            prioridad: '1'
-        }, {
-            nombre: 'Pasear al perro',
-            prioridad: '2'
-        }, {
-            nombre: 'Ir al cine',
-            prioridad: '0'
-        }]
+        comun.tareas = []
 
         comun.tarea = {};
-
-        comun.eliminar = function(tarea) {
-            var indice = comun.tareas.indexOf(tarea);
-            comun.tareas.splice(indice, 1);
+        
+        /*Métodos remotos, consumo REST*/
+        comun.getAll = function(){
+            return $http.get('/tareas')
+            .success(function(data){
+                angular.copy(data,comun.tareas);
+                return comun.tareas;
+            })
+        }
+        
+        comun.add = function(tarea){
+            return $http.post('/tarea/', tarea)
+            .success(function(res){
+                comun.tareas.push(res);
+            })
+        }
+        
+        comun.update = function(tarea){
+            return $http.put('/tarea/' + tarea._id, tarea)
+            .success(function(res){
+                var indice = comun.tareas.indexOf(tarea);
+                
+                comun.tareas[indice] = res;
+            })            
+        }
+        
+        comun.delete = function(tarea){
+            return $http.delete('/tarea/' + tarea._id)
+            .success(function(){
+                var i = comun.tareas.indexOf(tarea);
+            comun.tareas.splice(i, 1);
+            })
         }
 
         return comun;
     })
+
+
     .controller('ctrlAlta', function($scope, $state, comun) {
         $scope.tarea = {}
             // $scope.tareas = [];
+        comun.getAll();
 
         $scope.tareas = comun.tareas;
 
         $scope.prioridades = ['Baja', 'Normal', 'Alta'];
 
         $scope.agregar = function() {
-            $scope.tareas.push({
+            comun.add({
                 nombre: $scope.tarea.nombre,
                 prioridad: parseInt($scope.tarea.prioridad)
             })
@@ -64,7 +87,7 @@ angular.module('appTareas', ['ui.router'])
         }
 
         $scope.eliminar = function(tarea) {
-            comun.eliminar(tarea)
+            comun.delete(tarea)
         }
 
         $scope.procesaObjeto = function(tarea) {
@@ -77,13 +100,12 @@ angular.module('appTareas', ['ui.router'])
         $scope.tarea = comun.tarea;
         
         $scope.actualizar = function() {
-            var indice = comun.tareas.indexOf(comun.tarea);
-            comun.tareas[indice] = $scope.tarea;
+            comun.update($scope.tarea);
             $state.go('alta');
         }
 
         $scope.eliminar = function(){
-            comun.eliminar($scope.tarea);
+            comun.delete($scope.tarea);
             $state.go('alta');
         }
     })
